640d9087db355134c7a75443735719343cc21806
lucsbandeira06 <lucasbandeira06@gmail.com>
2023-04-12 00:00:02 +0100

    Final commit, Login and README

---
+++ README.md
@@ -0,0 +1,43 @@
+# CA4 - BACK END DEVELOPMENT #
+## Flask To-Do Application## ##
+### Lucas Bandeira - 23884 ###
+
+This is a to-do flask application connected with MongoDB, and flask user authentication. In order to run this application, you will have to install the following libraries
+
+- pip install flask pymongo flask-login
+
+The user should be able to login or register in the application. 
+They will also be able to create and delete to-do tasks. 
+These tasks will then be stored in a MongoDB cloud environment managed by the admin.
+
+Below you can see a screenshot of the home page of this application.
+
+![img.png](img.png)
+
+The user can then create an account within the application. It is important
+to mention that the to-do object will only be sent to the database if they are bound to a user account.
+
+![img_1.png](img_1.png)
+
+In case the user still has not created an account he can then navigate to route register.
+
+![img_2.png](img_2.png)
+
+Once they are registered and logged in. The user will be able to create and delete to-do tasks.
+
+![img_3.png](img_3.png)
+
+The user data is kept in the MongoDB along with his to-do tasks history.
+
+![img_4.png](img_4.png)
+
+As you may have noticed the password is encrypted for security purposes.
+
+![img_5.png](img_5.png)
+
+
+## References ##
+
+- SSL Certificate for MacOS - https://pymongo.readthedocs.io/en/stable/examples/tls.html
+- Flask and MongoDB tutorial - https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application
+- Flask Login - https://www.youtube.com/watch?v=71EU8gnZqZQ&t=585s
--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 from bson.objectid import ObjectId
 from pymongo import MongoClient
-from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from functools import wraps
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5ce4035efd7075109bc7139adaba2ff1c853035b'
 
 app.debug = True
 
+# Creating an instance of Flask_Bcrypt
+bcrypt = Bcrypt(app)
+
 # Setting up the MongoDB database connection
 client = MongoClient("mongodb+srv://lucasbandeira06:Password1@flaskapp.pajmk5z.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
 
 # Setting up the database and collections
 db = client.FlaskApp
 todos = db.ToDos
+users = db.users
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -23,11 +27,11 @@ def index():
     if request.method == 'POST':
         # Get the content, degree of priority, and due date from the form
         content = request.form['content']
-        degree = request.form['degree']
-        due_date = request.form['frequency']
+        importance = request.form['degree']
+        frequency = request.form['frequency']
 
         # Insert the new todos into the todos collection
-        todos.insert_one({'content': content, 'degree': degree, 'due_date': due_date})
+        todos.insert_one({'content': content, 'importance': importance, 'frequency': frequency})
 
         # Redirect to the index page to show the updated list of todos
         return redirect(url_for('index'))
@@ -49,5 +53,75 @@ def delete(id):
     return redirect(url_for('index'))
 
 
+# Login required decorator to restrict access to certain pages.
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('user') is None:
+            # Redirect the user to the login page if they're not logged in
+            return redirect(url_for('login', next=request.url))
+
+        # Call the original function if the user is logged in
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+# Login page when "localhost" is accessed.
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get the username and password from the login form
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Find the user in the users collection
+        user = users.find_one({'username': username})
+
+        if user is None:
+            # Return an error message if the username doesn't exist in the database
+            return 'Invalid username or password'
+
+        if bcrypt.check_password_hash(user['password'], password):
+            # Set the user session if the password is correct
+            session['user'] = username
+            # Redirect to the index page to show the list of todos
+            return redirect(url_for('index'))
+        # Return an error message if the password is incorrect
+        return 'Invalid username or password'
+    # Render the login.html template if the request is a GET request
+    return render_template('login.html')
+
+
+# Template render for the registration form
+@app.route('/register')
+def register_form():
+    return render_template('register.html')
+
+
+# Register user by username and password
+# handle the submission of the registration form
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # check if the username is already registered
+    user_registered = users.find_one({'username': username})
+
+    # if the users does not exit, create it
+    if user_registered is None:
+        passwordHash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        # add document to the database
+        users.insert_one({'username': username, 'password': passwordHash})
+
+        # redirect to login page
+        return redirect(url_for('login'))
+
+    # if username is already registered, do not register
+    return 'Username already registered!'.encode('utf-8')
+
+
 if __name__ == '__main__':
-    app.run(ssl_context=None)
+    app.run()
